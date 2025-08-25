193238ffb537a681ef2f6035398e849697e8c411
Angelica Chin Cantillo <69483578+ange07@users.noreply.github.com>
2025-08-25 12:30:35 -0600

    Agregue mi codigo del fizzbuzz.js

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,16 @@
-export function fizzbuzz(){
-  //tu código va aqui
-  
+export function fizzbuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    }
+    else if (i % 3 === 0) {
+      console.log('Fizz');
+    }
+    else if (i % 5 === 0) {
+      console.log('Buzz');
+    }
+    else {
+      console.log(i);
+    }
+  }
 }
